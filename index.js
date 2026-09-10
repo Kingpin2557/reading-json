983ae70029231e3918b2feb366d25b86cca13928
Kingpin2557 <lode.snaet@outlook.com>
2026-09-10 15:55:09 +0200

    Refactor file reader logic into Upload component

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 const json = document.querySelector("#json");
-// const preview = document.querySelector(".c-jsoncontainer");
+const preview = document.querySelector(".c-jsoncontainer");
 
 json.addEventListener("change", () => {
-  const fr = new FileReader();
 
-  fr.readAsText(json.files[0]);
-
-  fr.addEventListener("load", () => {
-    const data = JSON.parse(fr.result);
-
-    for (let key of Object.keys(data)) {
-      console.log(typeof data[key]);
-    }
-  });
 });
--- src/Components/Upload/Upload.jsx
+++ src/Components/Upload/Upload.jsx
@@ -1,10 +1,44 @@
+import { useState, useEffect } from "react";
+
 import "./Upload.css";
 
 function DataLoader() {
+  const [file, setFile] = useState();
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+    }
+  };
+
+  useEffect(() => {
+    if (!file) return;
+
+    const fr = new FileReader();
+
+    fr.readAsText(file);
+
+    fr.addEventListener("load", () => {
+      const data = JSON.parse(fr.result);
+
+      for (let key of Object.keys(data)) {
+        console.log(typeof data[key]);
+      }
+    });
+  }, [file]);
+
   return (
     <div className="c-jsonfile">
       <label htmlFor="json">Upload your json file</label>
-      <input id="json" name="json" type="file" accept=".json" hidden />
+      <input
+        id="json"
+        name="json"
+        type="file"
+        accept=".json"
+        hidden
+        onChange={handleFileChange}
+      />
     </div>
   );
 }
